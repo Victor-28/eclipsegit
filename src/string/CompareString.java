d279903485f6033f968ccff4cde44cbdc8fabbb7
Victor-28 <victorakintola@gmail.com>
2021-01-14 11:56:40 +0100

    my comparing string

---
+++ src/string/CompareString.java
@@ -0,0 +1,16 @@
+package string;
+
+public class CompareString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String P="Selenium class";
+String Q="Selenium Training";
+if(P.equals(0)){
+	System.out.println("P and Q are equal");
+}else {
+	System.out.println("P and Q are not equal");
+	
+}		
+	}
+}
